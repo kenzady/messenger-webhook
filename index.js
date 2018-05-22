959f54665db8d164f7394e4c6cc319e7141568a2
Kenza <kenza.dieye@gmail.com>
2018-05-22 20:23:22 +0200

    added the fb page token to the app

--- index.js
+++ index.js
@@ -2,9 +2,25 @@
 
 // Imports dependencies and set up http server
 const
+  PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
   express = require('express'),
   bodyParser = require('body-parser'),
   app = express().use(bodyParser.json()); // creates express http server
+
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+
+}
 
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
@@ -44,8 +60,8 @@ app.get('/', function (req, res) {
 app.get('/webhook', (req, res) => {
 
   // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = "leyachtignilife06"
-
+  //let VERIFY_TOKEN = "leyachtignilife06"
+  const VERIFY_TOKEN = "EAAFnRQ0j8QYBAJ6kbgzCAx8AO2jZCH9c6JVARkDlKTF6n7MzyQzNwF2uOqz59mxrv4H4ERZC7ZBmARRVNVBVXooDs4XcraT5WDoiuHGkqzim81FgMYKHNfI4oPunimWnfcdf9tS3wB2cJrhmWaNCm0szNW8vNqzZAhxHLNiWNAZDZD";
 //const VERIFY_TOKEN = "leyachtignilife06";
   //if (req.query['hub.verify_token'] === process.env.VERIFY_TOKEN){
   //  return res.send(req.query['hub.challenge'])
@@ -73,4 +89,34 @@ app.get('/webhook', (req, res) => {
     }
   }
 });
-// fix buidpacks
+
+app.post('/webhook', (req, res) => {
+
+  // Parse the request body from the POST
+  let body = req.body;
+
+  // Check the webhook event is from a Page subscription
+  if (body.object === 'page') {
+
+    // Iterate over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
+
+      // Gets the body of the webhook event
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+
+      // Get the sender PSID
+      let sender_psid = webhook_event.sender.id;
+      console.log('Sender PSID: ' + sender_psid);
+
+    });
+
+    // Return a '200 OK' response to all events
+    res.status(200).send('EVENT_RECEIVED');
+
+  } else {
+    // Return a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
+
+});
