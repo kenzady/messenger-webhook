987629e12c16c5e1930d839877854c1109bd71fc
Kenza <kenza.dieye@gmail.com>
2018-05-22 22:37:54 +0200

    building the first version of the bot

--- index.js
+++ index.js
@@ -6,10 +6,23 @@ const
   express = require('express'),
   bodyParser = require('body-parser'),
   app = express().use(bodyParser.json()); // creates express http server
-
+  request = require('request');
 // Handles messages events
 function handleMessage(sender_psid, received_message) {
 
+  let response;
+
+  // Check if the message contains text
+  if (received_message.text) {
+
+    // Create the payload for a basic text message
+    response = {
+      "text": `You sent the message: "${received_message.text}". Now send me an image!`
+    }
+  }
+
+  // Sends the response message
+  callSendAPI(sender_psid, response);
 }
 
 // Handles messaging_postbacks events
@@ -19,7 +32,27 @@ function handlePostback(sender_psid, received_postback) {
 
 // Sends response messages via the Send API
 function callSendAPI(sender_psid, response) {
+  // Construct the message body
+  let request_body = {
+    "recipient": {
+      "id": sender_psid
+    },
+    "message": response
+  }
 
+  // Send the HTTP request to the Messenger Platform
+  request({
+    "uri": "https://graph.facebook.com/v2.6/me/messages",
+    "qs": { "access_token": PAGE_ACCESS_TOKEN },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('message sent!')
+    } else {
+      console.error("Unable to send message:" + err);
+    }
+  });
 }
 
 // Sets server port and logs message on success
@@ -61,7 +94,7 @@ app.get('/webhook', (req, res) => {
 
   // Your verify token. Should be a random string.
   //let VERIFY_TOKEN = "leyachtignilife06"
-  const VERIFY_TOKEN = "EAAFnRQ0j8QYBAJ6kbgzCAx8AO2jZCH9c6JVARkDlKTF6n7MzyQzNwF2uOqz59mxrv4H4ERZC7ZBmARRVNVBVXooDs4XcraT5WDoiuHGkqzim81FgMYKHNfI4oPunimWnfcdf9tS3wB2cJrhmWaNCm0szNW8vNqzZAhxHLNiWNAZDZD";
+  const VERIFY_TOKEN = PAGE_ACCESS_TOKEN;
 //const VERIFY_TOKEN = "leyachtignilife06";
   //if (req.query['hub.verify_token'] === process.env.VERIFY_TOKEN){
   //  return res.send(req.query['hub.challenge'])
@@ -105,9 +138,18 @@ app.post('/webhook', (req, res) => {
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
 
+
       // Get the sender PSID
       let sender_psid = webhook_event.sender.id;
       console.log('Sender PSID: ' + sender_psid);
+
+      // Check if the event is a message or postback and
+      // pass the event to the appropriate handler function
+      if (webhook_event.message) {
+        handleMessage(sender_psid, webhook_event.message);
+      } else if (webhook_event.postback) {
+        handlePostback(sender_psid, webhook_event.postback);
+      }
 
     });
 
