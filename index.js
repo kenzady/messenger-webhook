b079f8f9598cff16725aebbfcb1f06d277785b0d
Kenza <kenza.dieye@gmail.com>
2018-05-23 00:58:09 +0200

    removing doublon post

--- index.js
+++ index.js
@@ -12,32 +12,6 @@ const
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
-// Creates the endpoint for our webhook
-app.post('/webhook', (req, res) => {
-
-  let body = req.body;
-
-  // Checks this is an event from a page subscription
-  if (body.object === 'page') {
-
-    // Iterates over each entry - there may be multiple if batched
-    body.entry.forEach(function(entry) {
-
-      // Gets the message. entry.messaging is an array, but
-      // will only ever contain one message, so we get index 0
-      let webhook_event = entry.messaging[0];
-      console.log(webhook_event);
-    });
-
-    // Returns a '200 OK' response to all requests
-    res.status(200).send('EVENT_RECEIVED');
-  } else {
-    // Returns a '404 Not Found' if event is not from a page subscription
-    res.sendStatus(404);
-  }
-
-});
-
 // Index route
 app.get('/', function (req, res) {
   res.send('Hello world, I am a chat bot')
@@ -77,6 +51,7 @@ app.get('/webhook', (req, res) => {
   }
 });
 
+// Creates the endpoint for our webhook
 app.post('/webhook', (req, res) => {
 
   // Parse the request body from the POST
