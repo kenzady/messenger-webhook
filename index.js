4fb68cdf57827a0361a0b0715372fecf9f9dff36
Kenza <kenza.dieye@gmail.com>
2018-05-23 01:28:38 +0200

    test with auriane get webhook settings

--- index.js
+++ index.js
@@ -17,41 +17,16 @@ app.get('/', function (req, res) {
   res.send('Hello world, I am a chat bot')
 });
 
-// Adds support for GET requests to our webhook
-app.get('/webhook', (req, res) => {
-
-  // Your verify token. Should be a random string.
-  //let VERIFY_TOKEN = "leyachtignilife06"
+app.get('/webhook/', function(req, res) {
   const VERIFY_TOKEN = "CHALLENGE_ACCEPTED";
-//const VERIFY_TOKEN = "leyachtignilife06";
-  //if (req.query['hub.verify_token'] === process.env.VERIFY_TOKEN){
-  //  return res.send(req.query['hub.challenge'])
-  //}
-  //res.send('wrong token')
-
-  // Parse the query params
-  let mode = req.query['hub.mode'];
-  let token = req.query['hub.verify_token'];
-  let challenge = req.query['hub.challenge'];
-
-  // Checks if a token and mode is in the query string of the request
-  if (mode && token) {
-
-    // Checks the mode and token sent is correct
-    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-
-      // Responds with the challenge token from the request
-      console.log('WEBHOOK_VERIFIED');
-      res.status(200).send(challenge);
-
-    } else {
-      // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);
-    }
+  if (req.query['hub.verify_token'] === process.env.VERIFY_TOKEN){
+    return res.send(req.query['hub.challenge'])
   }
+  res.send('wrong token')
 });
 
-// Creates the endpoint for our webhook
+
+// Creates the endpoint for our webhook with auriane settings
 app.post('/webhook', (req, res) => {
 
   // Parse the request body from the POST
@@ -139,3 +114,5 @@ function callSendAPI(sender_psid, response) {
     }
   });
 }
+
+//test
